bc8477130e3a6b7dea652431f21502eff83d59e8
John8Rider <connerrobert927@gmail.com>
2023-05-19 17:48:36 +0900

    dashboard changed

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,8 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Blockchain developer",
+          "Full stack developer",
         ],
         autoStart: true,
         loop: true,
