e1ba0728f9c8946fdae0e3bd71ce349406437a0a
John8Rider <connerrobert927@gmail.com>
2023-06-28 17:35:26 +0900

    fix: modify resume link

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Flyn_Lee-BIT_MESRA.pdf";
+import pdf from "./Flyn_Lee-BIT_MESRA.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
